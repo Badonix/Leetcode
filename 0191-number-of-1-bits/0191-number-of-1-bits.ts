a8b5de276cecd9d660d23fde2c0e171abd5f195b
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-02-27 22:17:00 +0400

    Time: 68 ms (2.77%), Space: 51.8 MB (45.66%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.ts
@@ -0,0 +1,10 @@
+function hammingWeight(n: number): number {
+    let binary = n.toString(2)
+    let count = 0
+    for(let i = 0; i < binary.length; i++){
+        if(binary[i] == "1"){
+            count++
+        }
+    }
+    return count
+};
