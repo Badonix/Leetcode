b337c4354091e193d793678b8dae063468698099
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-02-27 22:21:53 +0400

    Time: 68 ms (5.3%), Space: 52.8 MB (96.78%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.ts
@@ -0,0 +1,25 @@
+function topKFrequent(nums: number[], k: number): number[] {
+  let bucket = [];
+  let hashmap = {};
+  nums.forEach((el) => {
+    hashmap[el] = hashmap[el] != undefined ? hashmap[el] + 1 : 1;
+  });
+  for (let property in hashmap) {
+    if (bucket[hashmap[property]]) {
+      bucket[hashmap[property]].push(property);
+    } else {
+      bucket[hashmap[property]] = [property];
+    }
+  }
+  let arr = [];
+  bucket = bucket.flat();
+
+  while (k > 0) {
+    if (bucket[bucket.length - 1]) {
+      k--;
+      arr.push(bucket[bucket.length - 1]);
+      bucket.length -= 1;
+    }
+  }
+  return arr;
+};
