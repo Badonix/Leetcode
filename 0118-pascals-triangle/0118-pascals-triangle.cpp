9065410787be0d8cc4696be4663bb275f8c5d7fe
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-08-01 18:41:11 +0400

    Time: 2 ms (15.16%), Space: 9.6 MB (62.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        std::vector<std::vector<int>> res;
-        res.push_back({1});
+        vector<vector<int>> triangle;
+        triangle.push_back({1});
 
-        for (int i = 0; i < numRows - 1; i++) {
-            std::vector<int> dummyRow = {0};
-            dummyRow.insert(dummyRow.end(), res.back().begin(), res.back().end());
-            dummyRow.push_back(0);
-            std::vector<int> row;
+        for (int rowNum = 1; rowNum < numRows; ++rowNum) {
+            vector<int> row;
+            vector<int>& prevRow = triangle[rowNum - 1];
 
-            for (int j = 0; j < dummyRow.size() - 1; j++) {
-                row.push_back(dummyRow[j] + dummyRow[j + 1]);
+            row.push_back(1);
+
+            for (int j = 1; j < rowNum; ++j) {
+                row.push_back(prevRow[j - 1] + prevRow[j]);
             }
 
-            res.push_back(row);
+            row.push_back(1);
+            triangle.push_back(row);
         }
 
-        return res;        
+        return triangle;
     }
 };
