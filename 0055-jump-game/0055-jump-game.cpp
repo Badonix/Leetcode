a1bff5d1ec769dd0d1a3142de15de18290ab72a9
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-08-29 15:56:55 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canWin(vector<int>& nums, int index, vector<bool>& v){
+        if(v[index]) return false;
+        if(index + nums[index] >= nums.size()){
+            return true;
+        }
+        for(int i = 1; i <= nums[index]; i++){
+            v[index+i-1] = true;
+            if(canWin(nums, index + i, v)){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        if(nums.size() == 1) return true;
+        vector<bool> v(nums.size(), false);
+        return canWin(nums, 0, v);
+    }
+};
