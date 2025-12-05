9eae9c26c58b0e5e48e23c917ea9305504f51d0c
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-12-05 17:23:29 +0400

    Time: 0 ms (100%), Space: 42.9 MB (22.69%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,10 +1,12 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        boolean[] freq = new boolean[26];
+        int[] frequencyArray = new int[26];
         for(int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            if(freq[c-'a']) return c;
-            freq[c-'a'] = true;
+            frequencyArray[c-'a']++;
+            if(frequencyArray[c-'a'] == 2){
+                return c;
+            }
         }
         return s.charAt(0);
     }
