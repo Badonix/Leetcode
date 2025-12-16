694449b9b6483b3f2d9df43fc8a3e66d0f05006f
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2025-12-16 16:38:25 +0400

    Time: 865 ms (5.01%), Space: 134.3 MB (6.97%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,40 +1,54 @@
 class LRUCache {
 
-    private HashMap<Integer, Integer> map;
-    private ArrayList<Integer> order;
-    private int cap;
-    public LRUCache(int capacity) {
-        map = new HashMap<>();        
-        order = new ArrayList<>();        
-        cap = capacity;
+  private HashMap<Integer, Integer> map;
+  private ArrayList<Integer> order;
+  // {1: 1, 2: 2}
+  //
+  // [2, 1]
+
+  private int cap;
+
+  public LRUCache(int capacity) {
+    cap = capacity;
+    map = new HashMap<>();
+    order = new ArrayList<>();
+  }
+
+  public int get(int key) {
+    if (map.containsKey(key)) {
+      order.remove(Integer.valueOf(key));
+      order.add(key);
+      return map.get(key);
     }
-    
-    public int get(int key) {
-        if(map.containsKey(key)){
-            order.remove(Integer.valueOf(key));
-            order.add(key);
-            return map.get(key);
-        }        
-        return -1;
-    }
-    
-    public void put(int key, int value) {
-        if(map.containsKey(key)){
-            order.remove(Integer.valueOf(key));
-            order.add(key);
-            map.put(key, value);
-            return;
-        }
-        if(map.size() < cap){
-            map.put(key, value);
-            order.add(key);
-            return;
-        }
-        map.remove(order.get(0));
-        order.remove(0);
+    return -1;
+  }
+
+  public void put(int key, int value) {
+    if (map.size() < cap) {
+      if (map.containsKey(key)) {
+        map.put(key, value);
+        order.remove(Integer.valueOf(key));
+        order.add(key);
+      } else {
         map.put(key, value);
         order.add(key);
+      }
+      return;
     }
+
+    if (map.containsKey(key)) {
+      map.put(key, value);
+      order.remove(Integer.valueOf(key));
+      order.add(key);
+      return;
+    }
+
+    int toDelete = order.get(0);
+    map.remove(toDelete);
+    order.remove(0);
+    order.add(key);
+    map.put(key, value);
+  }
 }
 
 /**
