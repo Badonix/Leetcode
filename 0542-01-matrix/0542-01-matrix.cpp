c8603961e9fe7eccc9884739f2eb0367feb6025a
Nick Danelia <78652512+Badonix@users.noreply.github.com>
2026-04-18 16:39:22 +0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         queue<pair<int, int>> q;
-        set<pair<int, int>> visited;
         for(int i = 0; i < mat.size(); i++){
             for(int j = 0; j < mat[0].size(); j++){
                 if(mat[i][j] == 0) q.push({i, j});
+                if(mat[i][j] == 1) mat[i][j] = -1;
             }
         }
 
@@ -19,9 +19,8 @@ public:
                         if(abs(di) == abs(dj)) continue;
                         int newI = p.first + di;
                         int newJ = p.second + dj;
-                        if(newI < 0 || newJ < 0 || newI >= mat.size() || newJ >= mat[0].size() || visited.count({newI, newJ}) != 0) continue;
-                        if(mat[newI][newJ] == 1) mat[newI][newJ] = curr_distance;
-                        visited.insert({newI, newJ});
+                        if(newI < 0 || newJ < 0 || newI >= mat.size() || newJ >= mat[0].size() || mat[newI][newJ] != -1) continue;
+                        mat[newI][newJ] = curr_distance;
                         q.push({newI, newJ});
                     }
                 }
